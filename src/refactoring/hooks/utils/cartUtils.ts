4a664182717265bce0865c2c39d095e83fe3bc42
soyoonJ <sytkfkd96@gmail.com>
2024-10-20 16:18:33 +0900

    refactor: cartUtils.ts calculateItemTotal 함수

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,41 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon, Discount } from "../../../types";
 
+const getAppliedDiscount = (discounts: Discount[], quantity: number) => {
+	let appliedDiscount = 0;
+	for (const discount of discounts) {
+		if (quantity >= discount.quantity) {
+			appliedDiscount = Math.max(appliedDiscount, discount.rate);
+		}
+	}
+
+	return appliedDiscount;
+};
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+	const { price, discounts } = item.product;
+	const { quantity } = item;
+
+	const itemTotal = price * quantity;
+	const appliedDiscount = getAppliedDiscount(discounts, quantity);
+
+	return itemTotal * (1 - appliedDiscount);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+	return 0;
 };
 
 export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
-  return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
-  };
+	return {
+		totalBeforeDiscount: 0,
+		totalAfterDiscount: 0,
+		totalDiscount: 0,
+	};
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+export const updateCartItemQuantity = (
+	cart: CartItem[],
+	productId: string,
+	newQuantity: number
+): CartItem[] => {
+	return [];
 };
