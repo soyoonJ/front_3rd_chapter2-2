967bd46bc2a0f38e4877625e5e89dde29b4b5e35
soyoonJ <sytkfkd96@gmail.com>
2024-10-20 15:57:17 +0900

    refactor: useProduct.ts 함수 이동

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,16 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useState } from "react";
+import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+	const [products, setProducts] = useState<Product[]>(initialProducts);
+
+	const updateProduct = (product: Product) => {
+		setProducts((prevProducts) => prevProducts.map((p) => (p.id === product.id ? product : p)));
+	};
+
+	const addProduct = (product: Product) => {
+		setProducts((prevProducts) => [...prevProducts, product]);
+	};
+
+	return { products, updateProduct, addProduct };
 };
